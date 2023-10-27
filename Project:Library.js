cffbec222493fe6e7758ad013e5a1a8b6075cc71
Mouzone <sunnyliu010@gmail.com>
2023-10-27 14:37:36 -0700

    Created Repository and Added File

---
+++ Project:Library.js
@@ -0,0 +1,21 @@
+const myLibrary = [];
+
+function Book(title, author, pages, read){
+	this.title = title;
+	this.author = author;
+	this.pages = pages;
+	this.read = read;
+	
+	this.info = function(){
+		if (read){
+			return `${title} by ${author}, ${pages} pages`
+		}
+		else {
+			return `${title} by ${author}, ${pages} pages, not read yet`
+		}
+	}
+}
+
+function addBookToLibrary() {
+    
+}
